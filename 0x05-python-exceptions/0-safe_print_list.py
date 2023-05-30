352ac39530a5bc0bfb108a9163c111bd7fddad21
Chol Paul <cholthipaul@gmail.com>
2023-05-30 02:15:31 -0700

    Safe list printing with exceptions handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
         The number of elements printed.
     """
     ret = 0
-    for el in my_list:
+    for el in range(x):
         try:
             print("{}".format(my_list[el], end=""))
             ret += 1
