c18464b03fbf7c763bc1283ff71824f17ca26b63
root <cholthipaul@gmail.com>
2023-08-07 13:27:33 -0700

    Done with python tasks in this project

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""write a python function bytecode """
+
+def magic_calculation(a, b):
+    pass
---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+"""Prints a string followed by newline"""
+print('"Programming is like building a multilingual puzzle')
