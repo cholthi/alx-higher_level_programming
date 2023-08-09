f5b8f596ace8dfe80e84677a0c4a4288e8c0a9c6
root <cholthipaul@gmail.com>
2023-08-09 13:16:34 -0700

    correction for 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     """translate string to uppercase"""
-    lst = [chr(ord(c)-32) if ord(c) >= ord('a') and 
-            ord(c) <= ord('z') else c for c in str]
+    lst = [chr(ord(c)-32) if ord(c) >= ord('a') and
+          ord(c) <= ord('z') else c for c in str]
     print('{}'.format(''.join(lst)))
