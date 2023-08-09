b4acadb9f00cda3d874dd9aead6f464ba42f9053
root <cholthipaul@gmail.com>
2023-08-09 13:08:22 -0700

    correction for 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print('{}'.format(''.join(reversed([chr(c) if c % 2 == 0 else chr(c - 32)
-                             for c in range(97, 123)]))), end='')
+                                for c in range(97, 123)]))), end='')
