0e8f3516e9060d39f9e962f312e296690035e606
root <cholthipaul@gmail.com>
2023-08-10 14:24:21 -0700

    23-infinite_add.py  correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     """ Sums its agruments"""
     import sys
 
-    argv_int = map(lambda x: int(x), argv[1:])
+    argv_int = map(lambda x: int(x), sys.argv[1:])
     print('{}'.format(sum(argv_int)))
