d4c5bce7f7fc9792f4dcbb89583e1c902b7f0538
root <cholthipaul@gmail.com>
2023-08-13 01:34:06 -0700

    correction for 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,4 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """sums tuples args passed to it"""
-    all_tuples = (tuple_a, tuple_b)
-    return tuple((sum(t) for t in all_tuples))
+   return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
