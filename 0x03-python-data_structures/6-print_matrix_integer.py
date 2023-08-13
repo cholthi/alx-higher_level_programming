da29dc33b1f7ffd6ad262294342495477f5d50c3
root <cholthipaul@gmail.com>
2023-08-13 01:59:35 -0700

    correction for 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     """prints matrix of integers"""
-    if len(matrix) == 0:
-        print('')
-        return
-
     for i in matrix:
         for j in i:
             print('{:d}'.format(j), end='')
         print('')
+    print('')
