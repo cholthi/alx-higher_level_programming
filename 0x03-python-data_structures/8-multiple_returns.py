1f3528f7a9245a718e82ef69466b4966420d8a9d
root <cholthipaul@gmail.com>
2023-08-13 01:20:38 -0700

    correction for8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     """Returns length and first character of the argument in a tuple"""
-    if ''.__eq__(sentence):
+    if ''.__eq__(sentence.strip()):
         return ((0,None))
     return ((len(sentence), sentence[0]))
