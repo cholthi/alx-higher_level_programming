5d393a9683210756451fddcf3b3bda9da1454898
root <cholthipaul@gmail.com>
2023-08-13 01:00:58 -0700

    correction for 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     """Removes element at index idx from the list"""
-    if idx > len(my_list) or idx < 0:
+    if idx > len(my_list) - 1 or idx < 0:
         return (my_list)
     del my_list[idx]
     return (my_list)
