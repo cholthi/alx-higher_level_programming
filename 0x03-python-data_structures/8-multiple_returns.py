77b45f2c37f56bd9a7244c31576e448f1b728b1e
root <cholthipaul@gmail.com>
2023-08-14 01:24:09 -0700

    completed multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
     """Returns length and first character of the argument in a tuple"""
     if ''.__eq__(sentence.strip()):
-        return ((0,None))
+        return ((0, None))
     return ((len(sentence), sentence[0]))
