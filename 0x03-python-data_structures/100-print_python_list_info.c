cb7f1c5f3dec0e6e0414847af0a281d5d92a2631
root <cholthipaul@gmail.com>
2023-08-15 13:56:34 -0700

    correction for 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -19,6 +19,6 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %li\n", listobj->allocated);
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %i: = %s\n", i, Py_TYPE(listobj->ob_item[i])->tp_name);
+		printf("Element %i: %s\n", i, Py_TYPE(listobj->ob_item[i])->tp_name);
 	}
 }
