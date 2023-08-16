5dd7f7a6a8ec4f111064389cd388c5b1aea55565
root <cholthipaul@gmail.com>
2023-08-16 12:51:26 -0700

    correction for 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,4 @@
 
 def multiply_list_map(my_list=[], number=0):
     """maps values of a list my_list using mul function"""
-    return list(map(lambda x: x * number, my_list))
+    return list(map(lambda x: x * number, my_list.copy()))
