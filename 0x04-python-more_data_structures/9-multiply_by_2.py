e995c2a343e308be71368716d0118ebe73a85412
root <cholthipaul@gmail.com>
2023-08-16 13:06:53 -0700

    correction for 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,5 @@
 
 def multiply_by_2(a_dictionary):
     """multple dict values by 2"""
-   return ({k:v * 2 for k, v in a_dictionary.items()})
+    d = {k: v * 2 for k, v in a_dictionary.copy().items()}
+    return (d)
