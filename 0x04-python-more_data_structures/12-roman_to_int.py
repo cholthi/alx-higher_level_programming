6bb003970324ec3110afca4a88c0680de4e43a71
root <cholthipaul@gmail.com>
2023-08-16 12:45:11 -0700

    correction for 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,9 @@
 
 def roman_to_int(roman_string):
     """Roman to Dec """
-    roman_map = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,
-            'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+                 'CD': 400, 'CM': 900}
     dec = 0
     i = 0
 
