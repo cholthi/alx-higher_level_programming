21ed36e290575b5a2145670c4dc0573fbc3dbb03
root <cholthipaul@gmail.com>
2023-08-16 13:25:42 -0700

    correction for 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     """computes squares of the matrix values and return matrix"""
     new_matrix = []
     for i in matrix:
-       new_matrix.append([v * v for v in i])
-    return new_matrix
+        new_matrix.append([v * v for v in i])
+    return (new_matrix)
