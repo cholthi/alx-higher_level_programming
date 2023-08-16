74dabdd95c93dbd6ef602b9314b47f5762290f4c
root <cholthipaul@gmail.com>
2023-08-16 13:02:00 -0700

    correction for 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

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,11 @@
 def best_score(a_dictionary):
     """Returns the key with max value"""
     max = (2 ** 63 - 1) * -1
-    for k in a_dictionary:
-        if a_dictionary[k] > max:
-            key = k
-            max = a_dictionary[key]
-    return (key)
+    if not a_dictionary:
+        return (None)
+    else:
+        for k in a_dictionary:
+            if a_dictionary[k] > max:
+                key = k
+                max = a_dictionary[key]
+        return (key)
