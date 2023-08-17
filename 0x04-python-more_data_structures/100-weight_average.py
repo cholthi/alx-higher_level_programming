b449f87873e333d0e8ae12c56dda93a2921ec11f
root <cholthipaul@gmail.com>
2023-08-17 13:29:31 -0700

    correctiom for 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Returns weighted average of the input list"""
+    res = sum([a * b for a, b in my_list]) / sum(dict(my_list).values())
+    return (res)
