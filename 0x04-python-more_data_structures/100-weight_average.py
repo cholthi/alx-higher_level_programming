adb925ab9726dd836b12e25e26b68fcde07001ed
root <cholthipaul@gmail.com>
2023-08-17 13:37:33 -0700

    correctiom for 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     """Returns weighted average of the input list"""
+    if len(my_list) == 0:
+        return (0)
+
     res = (sum([a * b for a, b in my_list])) / (sum(dict(my_list).values()))
     return (res)
