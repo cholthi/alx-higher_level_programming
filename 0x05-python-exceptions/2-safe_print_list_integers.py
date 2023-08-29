1173979320e3c02042fb373a52b4a23792e4d3c3
Chol Paul <cholthipaul@gmail.com>
2023-08-29 00:15:10 -0700

    correction for 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     len = 0
+    if not my_list and not x:
+        return (0)
     try:
         for i in range(0, x):
             if isinstance(my_list[i], int):
