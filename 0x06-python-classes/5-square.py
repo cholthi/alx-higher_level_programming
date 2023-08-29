442b00a7ff069da18674ef3f59f8d5143f799827
Chol Paul <cholthipaul@gmail.com>
2023-08-29 13:23:13 -0700

    correction for many files

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -13,18 +13,20 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         return (self.__size * self.__size)
 
     def size(self):
         return (self.__size)
 
-    def size(self,value):
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def my_print(self):
         if self.__size == 0:
             print()
