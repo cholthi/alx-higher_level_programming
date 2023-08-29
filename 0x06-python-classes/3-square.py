926677bb3de2b9dfa806ece5fabd59e415b91c4a
Chol Paul <cholthipaul@gmail.com>
2023-08-29 13:02:52 -0700

    correction for many files

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,6 +4,7 @@ A square class to represent square shapes
 with private instance attribute and validation
 """
 
+
 class Square:
     """Square class for square shapes"""
     def __init__(self, size=0):
@@ -12,5 +13,6 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         return (self.__size * self.__size)
