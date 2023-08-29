3e250607be04b58682238f998af86b223fa920e9
Chol Paul <cholthipaul@gmail.com>
2023-08-29 12:48:04 -0700

    correction for 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-A square class to represent square shapes
-"""
+""" A square class to represent square shapes """
 class Square:
+    """ Square class for square shapes"""
     pass
