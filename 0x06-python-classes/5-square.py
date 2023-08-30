6ca9d41abdbf148ca1eb703345b19a217ad8dad4
Chol Paul <cholthipaul@gmail.com>
2023-08-30 04:06:47 -0700

    correction for 6-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -35,7 +35,4 @@ class Square:
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print('#', end='')
-            if i == self.__size - 1:
-                print(end='')
-            else:
-                print()
+            print()
