94344471945b7870d2f30419edc84ef385fec756
Chol Paul <cholthipaul@gmail.com>
2023-08-30 03:42:50 -0700

    correction for 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ with private instance attribute and validation
 
 class Square:
     """Square class for square shapes"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
