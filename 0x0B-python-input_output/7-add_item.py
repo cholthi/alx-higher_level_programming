168d5af80f49048bdbcd2a216482637c3c338b5e
Chol Paul <cholthipaul@gmail.com>
2023-09-12 12:57:19 -0700

    correction for 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Reads and writes to a json encoded file"""
+
+
+from sys import agrv
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        my_obj = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        my_obj = []
+    my_obj.extend(argv[1:])
+    save_to_json_file(my_obj, 'add_item.json')
