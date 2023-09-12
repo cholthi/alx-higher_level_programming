129b8a55621c382c63f46912d9a43c9ebb23a09b
Chol Paul <cholthipaul@gmail.com>
2023-09-12 11:57:19 -0700

    correction for def to_json_string(my_obj):

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Serializes python objects to json string"""
+
+
+from json import dumps
+def to_json_string(my_obj):
+    """
+    encodes a python object obj to json string
+
+    Args:
+        my_obj (any): Python object to encode to json
+    Returns:
+           str: A json string representation of an object
+    """
+    json_str = dumps(my_obj)
+    return (json_str)
