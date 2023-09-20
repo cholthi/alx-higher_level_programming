7fc9e8bfdcc321476c34e7cc45341ea675aa84bc
Chol Paul <cholthipaul@gmail.com>
2023-09-20 10:33:03 -0700

    correction for rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,7 +35,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         '''width value mutator'''
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError('width must be an integer')
         if value <= 0:
             raise valueError('width must be > 0')
@@ -49,7 +49,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         '''height value mutator'''
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError('height must be an integer')
         if value <= 0:
             raise ValueError('height  must be > 0')
@@ -63,7 +63,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         '''x value mutator'''
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError('x must be an integer')
         if value <= 0:
             raise ValueError('x must be > 0')
@@ -77,7 +77,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         '''y value muttator'''
-        if not type(value) == int:
+        if type(value) != int:
             raise TypeError('y must be an integer')
         if value <= 0:
             raise ValueError('y must be > 0')
