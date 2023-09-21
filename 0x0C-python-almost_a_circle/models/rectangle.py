e5dea03218786e90a3623930026e9a6d07fd6e3a
Chol Paul <cholthipaul@gmail.com>
2023-09-21 04:42:17 -0700

    correction for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,33 +109,49 @@ class Rectangle(Base):
                 f'/{self.__y} - {self.__width}/{self.__height}')
 
     def update(self, *args, **kwargs):
-        '''Updates attributes of this class instance'''
-        if args and len(args) != 0:
-            if args[0] is None:
-                self.__init__(
-                        self.width, self.height, self.x, self.y)
-            else:
-                self.id = args[0]
+        """Update the Rectangle.
 
-                self.width = args[1]
-                self.height = args[2]
-                self.x = args[3]
-                self.y = args[4]
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == 'id':
+                if k == "id":
                     if v is None:
-                        self.__init__(
-                            self.width, self.height, self.x, self.y)
+                        self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = v
-                elif k == 'width':
+                elif k == "width":
                     self.width = v
-                elif k == 'height':
+                elif k == "height":
                     self.height = v
-                elif k == 'x':
+                elif k == "x":
                     self.x = v
-                elif k == 'y':
+                elif k == "y":
                     self.y = v
 
     def to_dictionary(self):
