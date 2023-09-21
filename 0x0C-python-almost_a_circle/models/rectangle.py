5d4246ec0d7a60cb8e5e36fa426412304003c897
Chol Paul <cholthipaul@gmail.com>
2023-09-21 04:18:08 -0700

    correction for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,8 +23,8 @@ class Rectangle(Base):
         '''
         self.width = width
         self.height = height
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
