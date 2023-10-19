7d209a6a16cd9b157f672661f1cd70c707d069ad
cholthi <cholthipaul@gmail.com>
2023-10-19 11:32:15 +0300

    correction for 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
 -- Convert the encoding of hbtn_0c_0 to utf8mb4 and collate to utf8mb4_unicode_ci
+USE `hbtn_0c_0`
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
