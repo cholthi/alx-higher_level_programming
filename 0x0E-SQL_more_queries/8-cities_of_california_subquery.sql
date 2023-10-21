2aa676c010827d19b95e35bd01fc2c7ff0b7b214
cholthi <cholthipaul@gmail.com>
2023-10-21 11:22:05 +0300

    correction for 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,11 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT c.`name`
+SELECT `id`, `name`
 FROM
-`cities` AS c
-INNER JOIN `states` AS s
-ON c.`state_id` = s.`id`
-WHERE
-s.`name` = "California"
-ORDER BY c.`id`;
+`cities`
+WHERE `state_id` IN
+(SELECT `id`
+	FROM `states`
+	WHERE
+	`name` = "California"
+)
+ORDER BY `id`;
