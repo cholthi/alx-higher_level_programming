5a52adf6c7e380005d2c7340f8a49f55ac6f1dac
cholthi <cholthipaul@gmail.com>
2023-10-21 11:27:07 +0300

    correction for hbtn_0d_usa

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- Create a database hbtn_0d_usa and table states in it
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities`(
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.cities`(
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
 	`name` VARCHAR(256) NOT NULL,
 	`state_id` INT NOT NULL,
