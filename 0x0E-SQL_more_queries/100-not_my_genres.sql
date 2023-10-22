aede2d7437d259b832b74a64cb6883ecc53ca1ea
cholthi <cholthipaul@gmail.com>
2023-10-22 11:38:51 +0300

    correction for 100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Lists all genres not linked to the show Dexter
+-- Select column genre.name only
+-- Order rows by genre.name by asc
+SELECT `g.name`
+FROM `tv_genres` AS g
+INNER JOIN `tv_show_genres` sg
+ON g.`id` = sg.`genre_id`
+WHERE sg.`show_id` NOT IN (
+	SELECT `id`
+	FROM `tv_shows`
+	WHERE `title` != "Dexter"
+)
+ORDER BY g.`name`;
