cdd32483cef94c311ae72c7171b504ebca374977
cholthi <cholthipaul@gmail.com>
2023-10-23 10:38:43 +0300

    correction for 101-not_a_comedy.sql'

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,20 +1,13 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 -- Select column tv_show.title only
 -- Order rows by show.title by asc
-SELECT DISTINCT `title`
-  FROM `tv_shows` AS s
-       INNER JOIN `tv_show_genres` AS sg
-       ON s.`id` = sg.`genre_id`
-
-       INNER JOIN `tv_genres` AS g
-       ON sg.`genre_id` = g.`id`
-       WHERE s.`title` NOT IN
-             (SELECT `title`
-                FROM `tv_shows` AS s
-	             INNER JOIN `tv_show_genres` AS sg
-		     ON s.`id` = sg.`show_id`
-
-		     INNER JOIN `tv_genres` AS g
-		     ON sg.`show_id` = g.`id`
-		     WHERE g.`name` = "Comedy")
- ORDER BY s.`title`;
+SELECT *
+FROM `tv_shows` s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM `tv_show_genres` sg
+    JOIN `tv_genres` g ON sg.`genre_id` = g.id
+    WHERE sg.`show_id` = s.`id`
+    AND g.`name` = 'Comedy'
+)
+ORDER BY title;
