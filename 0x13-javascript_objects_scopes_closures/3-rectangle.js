0d80052343a648209e6eb0197870c20fd73544d9
cholthi <cholthipaul@gmail.com>
2023-11-16 16:44:33 +0200

    aaded advanced tasks solutions

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+	
+  print () {
+    for (let y = 0; y < this.height; y++) {
+        console.log('X'.repeat(this.width));
+    }
+  }
+};
