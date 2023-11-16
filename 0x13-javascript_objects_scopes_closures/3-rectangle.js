0f7c0b02ccaf52bdd0142204eb469fcd3ddf2471
cholthi <cholthipaul@gmail.com>
2023-11-16 16:55:06 +0200

    fixed semistandard violations

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,10 +3,8 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-	
+
   print () {
-    for (let y = 0; y < this.height; y++) {
-        console.log('X'.repeat(this.width));
-    }
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
