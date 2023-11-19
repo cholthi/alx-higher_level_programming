d465375932005d2e0bb63d32907bc7d1743d6853
cholthi <cholthipaul@gmail.com>
2023-11-19 11:27:17 +0200

    correction for 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT c.name FROM cities AS c inner join states
-            AS s on c.state_id = s.id where s.name = %s
+            AS s on c.state_id = s.id where s.name =%s
             order by c.id""", (sys.argv[4], ))
     rows = cur.fetchall()
     ltemp = [row[0] for row in rows]
