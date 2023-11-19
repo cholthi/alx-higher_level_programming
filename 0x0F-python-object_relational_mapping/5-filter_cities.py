4cf280aeb4a8e2445b9a302b451eff5088b60993
cholthi <cholthipaul@gmail.com>
2023-11-19 11:40:37 +0200

    correction for 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,12 +7,12 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT c.name FROM cities AS c inner join states
-            AS s on c.state_id = s.id where 
-            s.name =%s""", (sys.argv[4], ))
+            AS s on c.state_id = s.id where
+            s.name =%s""", (sys.argv[4],))
     rows = cur.fetchall()
     temp = [row[0] for row in rows]
     print(*temp, sep=", ")
