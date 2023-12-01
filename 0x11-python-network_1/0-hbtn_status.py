e099d75ab2585fd22d2391ff87d57df20d3f9407
cholthi <cholthipaul@gmail.com>
2023-12-01 11:25:05 +0200

    added 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches content from remote url and formats
+It in a certain format
+"""
+
+from urllib import request
+
+url = 'https://alx-intranet.hbtn.io/status'
+with request.urlopen(url) as response:
+    data = response.read()
+    _type = type(data)
+    decoded_utf8 = data.decode('utf8')
+    print(f"Body response:\n\t- type: {_type}\n\t- content: {data}"""
+            f"\n\t- utf8 content: {decoded_utf8}""")
