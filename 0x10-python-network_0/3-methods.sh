10f27ff8e1496539280d8bfa19e66b41e6158d46
cholthi <cholthipaul@gmail.com>
2023-12-01 09:58:30 +0200

    correction for 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Extracts list of supported http methods by a url
-curl -sI "$1" | grep -i Allow | awk -F ':' '{print $2}'
+curl -sI "$1" | grep -i Allow | awk -F ':' '{print $2}' | head -c -2 | rev | head -c -1 | rev
