609592576b7e0ed0a0c89854c67d3b90ef99bd73
cholthi <cholthipaul@gmail.com>
2023-12-02 15:00:24 +0200

    added 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,18 +2,17 @@
 """ Receives a username and password for use with GithubAPI
 Retrives the github user information
 """
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    user = argv[1]
+    passwd = argv[2]
+    endpoint = 'https://api.github.com/user'
 
-import requests
-from sys import argv
+    response = requests.get(endpoint, auth=(user, passwd))
 
-user = argv[1]
-passwd = argv[2]
-endpoint = 'https://api.github.com/user'
-
-response = requests.get(endpoint, auth=(user, passwd))
-
-if response.status_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         data = response.json()
-        print(data['id'])
-else:
-    print(None)
+        print(data.get('id'))
+    else:
+        print(None)
